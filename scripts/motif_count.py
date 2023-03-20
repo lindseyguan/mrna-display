41f2ae6d14b70633423585b9eae52a422e70c28a
Lindsey Guan <lguan@mit.edu>
2023-03-19 21:46:49 -0400

    implemented n-distance motif counting

--- scripts/motif_count.py
+++ scripts/motif_count.py
@@ -5,9 +5,12 @@ Calculates motif enrichment.
 import os
 import sys
 
+import numpy as np
 import pandas as pd
+from tqdm.notebook import tqdm
 
 sys.path.insert(0, os.path.abspath('../'))
+
 
 # Calculate monomer frequencies from codon table
 codons = pd.read_csv('../ngs/codon_box.csv')
@@ -22,6 +25,7 @@ for i, i_freq in monomers_freq_dict.items():
     for j, j_freq in monomers_freq_dict.items():
         pairs_freq_dict[i+j] = i_freq * j_freq
 
+        
 # Monomers
 
 for selection in range(1, 5):
@@ -31,7 +35,7 @@ for selection in range(1, 5):
             reads.append(line.strip())
     s = ''.join(reads)
     freqs = {}
-
+    
     # Normalize by frequency of occurrence in codon table
     for m, m_freq in monomers_freq_dict.items():
         freqs[m] = s.count(m) / m_freq
@@ -40,6 +44,7 @@ for selection in range(1, 5):
     df['Frequency'] = df['Frequency'] / df['Frequency'].sum()
     df.to_csv(f'../seq/monomer/S{selection}_monomer_freq.txt', index=None)
 
+    
 # Pairs
 
 for selection in range(1, 5):
@@ -53,11 +58,35 @@ for selection in range(1, 5):
                     pair_counts[pep[i:i+2]] += 1
                 else:
                     pair_counts[pep[i:i+2]] = 1
-
+                    
     for p, p_freq in pair_counts.items():
         pair_counts[p] = p_freq / pairs_freq_dict[p]
-    df = pd.DataFrame(zip(list(pair_counts), list(pair_counts.values())),
-                      columns=['Pair', 'Frequency'])
+    df = pd.DataFrame(zip(list(pair_counts), list(pair_counts.values())), columns=['Pair', 'Frequency'])
     df['Frequency'] = df['Frequency'] / df['Frequency'].sum()
     df.to_csv(f'../seq/pair/S{selection}_pair_freq.txt', index=None)
+
     
+# N-distance
+
+for selection in range(1, 5):
+    motif_counts = {}
+    with open(f'../seq/S{selection}_peptide.txt', 'r') as f:
+        num_lines = sum(1 for line in open(f'../seq/S{selection}_peptide.txt','r')) - 1
+        next(f) # Skip first line
+        for line in tqdm(f, total=num_lines):
+            pep = line.strip()
+            for i in range(len(pep) - 1):
+                left = pep[i]
+                for j in range(1, len(pep) - i):
+                    right = pep[i+j]
+                    rep = f'{left}{j-1}{right}'
+                    if rep in motif_counts:
+                        motif_counts[rep] += 1
+                    else:
+                        motif_counts[rep] = 1
+    for motif, motif_count in motif_counts.items():
+        pair_key = motif[0] + motif[-1]
+        motif_counts[motif] = motif_count / pairs_freq_dict[pair_key]
+    df = pd.DataFrame(zip(list(motif_counts), list(motif_counts.values())), columns=['Motif', 'Frequency'])
+    df['Frequency'] = df['Frequency'] / df['Frequency'].sum()
+    df.to_csv(f'../seq/n_dist/S{selection}_ndist_freq.txt', index=None)
