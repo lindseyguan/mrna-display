1f33ae077c816ccedd9193697cf8cd9e5d2edc33
Lindsey Guan <lguan@mit.edu>
2023-04-04 13:32:07 -0400

    moved base classifier

--- modeling/mrna_classifier.py
+++ modeling/mrna_classifier.py
@@ -5,6 +5,27 @@ from sklearn.metrics import roc_auc_score
 from torch.utils.data import DataLoader, SubsetRandomSampler
 from mrna_dataset import MrnaDisplayDataset
 
+class MrnaBaseClassifier(torch.nn.Module) :
+    def __init__(self, input_dim, hidden_dim) :
+        super().__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        # Define a MLP regressor with 2 hidden layers
+        self.model = nn.Sequential(nn.Linear(self.input_dim, self.hidden_dim),
+                                   nn.ReLU(),
+                                   nn.Linear(self.hidden_dim, self.hidden_dim),
+                                   nn.ReLU(),
+                                   nn.Linear(self.hidden_dim, 1)
+                                  )
+        
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        output = self.model(x)
+        proba = self.sigmoid(output) # Convert output to probability
+        return proba
+        
 class MrnaBaggingPuClassifier:
     """
     Based on bagging-based positive-unlabeled learning model 
